07561d49354a2b74aee2c0edbaf2575f24267de2
camilo040494 <camilo040494@gmail.com>
2017-11-14 14:43:22 -0500

    Update configure-hadoop.sh

--- HadoopCluster/configure-hadoop.sh
+++ HadoopCluster/configure-hadoop.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
-wget hadoop sources
-install protoc-buffer
+curl hadoop sources
+
+install protoc-buffer:
+apt-get update
+apt-get upgrade
+apt-get install -y gcc g++
+curl -O https://github.com/google/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.gz
+tar -xzvf 
+cd protobuf-2.5.0.tar.gz
+sudo ./configure
+sudo make
+sudo make check
+sudo make install
+
+
 cp jars
 cp eketal file
 
