7df4480ae993ae1ce051db379d68feb99c1cf227
Camilo Pimienta Rosero <cfpimienta@icesi.edu.co>
2017-11-25 16:39:14 -0500

    Updated the eketal builder and the hadoop builder
    Eketal can be built from sources, as hadoop, hadoop will install c and c++ compilers, install the protocolbuffer and download hadoop sources, then, it will copy necessary things and can built the yarn example

--- HadoopCluster/configure-eketal.sh
+++ HadoopCluster/configure-eketal.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-
+echo "Building EKETAL"
 git clone -b master https://github.com/unicesi/eketal.git
 cd eketal/co.edu.icesi.eketal.parent-master
 mvn clean install -DskiptTests=true
 sudo echo "export EKETAL_HOME=/home/vagrant/eketal/" >> /home/vagrant/.bash_profile
 cp $EKETAL_HOME/co.edu.icesi.eketal.lib/target/*jar $HADOOP_COMMON_HOME/share/hadoop/yarn/lib;
+echo "EKETAL built succesfully"
--- HadoopCluster/configure-hadoop.sh
+++ HadoopCluster/configure-hadoop.sh
@@ -1,24 +1,42 @@
 #!/usr/bin/env bash
 
-curl hadoop sources
-
 ####install protoc-buffer:
 #apt-get update
 #apt-get upgrade
 #apt-get install -y gcc g++
-yum install gcc-c++
-curl -O https://github.com/google/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.gz
-tar -xzvf protobuf-2.5.0.tar.gz
+echo "Installing C and C++ compilers"
+sudo yum update -y
+sudo yum install -y gcc gcc-c++
+
+#yum install -y wget
+#wget 
+
+echo "Downloading Protocolbuffer"
+curl -L -O https://github.com/google/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.gz
+sudo tar -xzvf protobuf-2.5.0.tar.gz
 cd protobuf-2.5.0
+echo "Building Protocolbuffer"
 sudo ./configure
 sudo make
 sudo make check
 sudo make install
+echo "Built of Protocolbuffer successfully end"
 
+#cp jars
+#cp eketal file y pom
 
-cp jars
-cp eketal file
+echo "Downloading Hadoop"
+curl -L -O https://github.com/apache/hadoop/archive/rel/release-2.7.3.tar.gz
+sudo tar -xzvf release-2.7.3.tar.gz
+cd hadoop-rel-release-2.7.3
+sudo unalias cp #This removes the alias of cp that root users have (the alias is cp -i), so everytime a cp is used, it will ask when overring, with this command, that alias will not longer exist
+# $which cp
+#  alias cp='cp -i'
+#          /usr/bin/cp
 
-mvn clean package -DskipTests=true
-cp yarn-servernode $HADOOP
-rsync
+#sudo cp directory/to/pom.xml hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/
+#sudo cp directory/to/NodeManager.eketal hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/
+
+#mvn clean package -DskipTests=true
+#cp yarn-servernode $HADOOP
+#rsync
