ab1672e62833a8bc30cdd47f6adba76ef021e7cd
Camilo Pimienta Rosero <cfpimienta@icesi.edu.co>
2017-11-30 14:48:41 -0500

    Change configurations to a better deploy

--- HadoopCluster/configure-eketal.sh
+++ HadoopCluster/configure-eketal.sh
@@ -2,8 +2,7 @@
 echo "Building EKETAL"
 git clone -b master https://github.com/unicesi/eketal.git
 cd eketal/co.edu.icesi.eketal.parent-master
-#mvn clean install -DskiptTests=true
+sudo -u root mvn clean install -DskiptTests=true
 sudo touch /etc/profile.d/eketal.sh
 sudo echo "export EKETAL_HOME=/home/vagrant/eketal/" >> /etc/profile.d/eketal.sh
-#cp $EKETAL_HOME/co.edu.icesi.eketal.lib/target/*jar $HADOOP_COMMON_HOME/share/hadoop/yarn/lib;
 echo "EKETAL built succesfully"
--- HadoopCluster/install-hadoop.sh
+++ HadoopCluster/install-hadoop.sh
@@ -18,15 +18,28 @@ export PATH=$PATH:$HADOOP_COMMON_HOME/sbin
 export HADOOP_PREFIX=$HADOOP_COMMON_HOME
 EOL
 
+sudo source /etc/profile.d/eketal.sh
+sudo source /etc/profile.d/hadoop.sh
 
-for i in `cat /etc/hosts | tail -n +4  | awk '{print $2}'`; do sudo echo -e $1 >> $HADOOP_COMMON_HOME/etc/hadoop/slaves
-done
+#Cuando tail -n es igual a 4 incluye el master, cuando es 5 no lo incluye al master
+cat /etc/hosts | tail -n +4  | awk '{print $2}' >> $HADOOP_COMMON_HOME/etc/hadoop/slaves
+#for i in `cat /etc/hosts | tail -n +4  | awk '{print $2}'`; do sudo echo -e $1 >> $HADOOP_COMMON_HOME/etc/hadoop/slaves
+#done
 
-#cd /home/vagrant/hadoop-rel-release-2.7.3
-#cp hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/target/*.jar $HADOOP_COMMON_HOME/share
+cd /home/vagrant/hadoop-rel-release-2.7.3
+cp hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/target/*.jar $HADOOP_COMMON_HOME/share/hadoop/yarn
+cd .m2/repository/repository/
+cp dk/brics/automaton/automaton/1.11-8/*.jar $HADOOP_COMMON_HOME/share/hadoop/yarn/lib
+cp org/aspectj/aspectjrt/1.8.9/*.jar $HADOOP_COMMON_HOME/share/hadoop/yarn/lib
+cp org/jgroups/jgroups/3.6.14.Final/*.jar $HADOOP_COMMON_HOME/share/hadoop/yarn/lib
+cp $EKETAL_HOME/co.edu.icesi.eketal.lib/target/*jar $HADOOP_COMMON_HOME/share/hadoop/yarn/lib
+
+#################
+#Falta los demas archivos del $HADOOP_COMMON_HOME/etc/hadoop/
+#################
 
 exec 3>&1 1>~/logs
-for i in `cat ${HADOOP_COMMON_HOME}/etc/hadoop/slaves`; do echo $i; rsync -avxP --exclude=logs $HADOOP_COMMON_HOME/ $i:/usr/lib/hadoop/;
+for i in `cat ${HADOOP_COMMON_HOME}/etc/hadoop/slaves | head -n -1`; do echo $i; rsync -avxP --exclude=logs $HADOOP_COMMON_HOME/ $i:/usr/lib/hadoop/;
 done
 exec 1>&3 3>&-;
 
