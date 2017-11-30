101bd4eae3923397251555b6938d8898d7de8487
camilopimienta <1107082145@icesi.edu.co>
2017-11-30 14:52:02 -0500

    Latest Changes

--- HadoopCluster/configure-hadoop.sh
+++ HadoopCluster/configure-hadoop.sh
@@ -44,23 +44,25 @@ else
  cp $HADOOP_SRC /home/vagrant/release-2.7.3.tar.gz
 fi
 
+cd /home/vagrant
 sudo tar -xzvf release-2.7.3.tar.gz
 cd hadoop-rel-release-2.7.3
 
 #If retorna algo, unalias,sino siga igual
-which cp | grep "cp -i"
-
-sudo unalias cp #This removes the alias of cp that root users have (the alias is cp -i), so everytime a cp is used, it will ask when overring, with this command, that alias will not longer exist
+CP_ALIAS=$(which cp | grep "cp -i")
+if [ ! -z $CP_ALIAS ]; then
+ sudo unalias cp #This removes the alias of cp that root users have (the alias is cp -i), so everytime a cp is used, it will ask when overring, with this command, that alias will not longer exist
 # $which cp
 #  alias cp='cp -i'
 #          /usr/bin/cp
+fi
 
 
 echo "Copying required files into sources of hadoop"
-#sudo cp directory/to/pom.xml hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/
-#sudo cp directory/to/NodeManager.eketal hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/
+sudo cp /vagrant/pom.xml hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/
+sudo cp /vagrant/NodeManager.eketal hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/
 
 echo "Building required jars from sources"
-#sudo mvn clean package -DskipTests=true
+sudo mvn clean package -DskipTests=true
 
 echo "Built of sources successfully ended"
