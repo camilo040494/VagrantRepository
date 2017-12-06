e69ea96c51e42258786a8a50ac01367e9c09f401
camilopimienta <1107082145@icesi.edu.co>
2017-12-06 10:06:06 -0500

    Correctly provisionning of the environment

--- HadoopCluster/install-hadoop.sh
+++ HadoopCluster/install-hadoop.sh
@@ -12,10 +12,10 @@ else
 fi
 
 sudo tar -xzvf hadoop-2.7.3.tar.gz -C /home/vagrant
-sudo mv /home/vagrant/hadoop-2.7.3 /home/vagrant/hadoop
+sudo mv /home/vagrant/hadoop-2.7.3 /usr/lib/hadoop
 sudo touch /etc/profile.d/hadoop.sh
 sudo cat >/etc/profile.d/hadoop.sh <<EOL
-HADOOP_COMMON_HOME=/home/vagrant/hadoop
+HADOOP_COMMON_HOME=/usr/lib/hadoop
 HADOOP_MAPRED_HOME=\$HADOOP_COMMON_HOME
 HADOOP_HDFS_HOME=\$HADOOP_COMMON_HOME
 YARN_HOME=\$HADOOP_COMMON_HOME
@@ -37,6 +37,8 @@ cat /etc/hosts | tail -n +4  | awk '{print $2}' > $HADOOP_COMMON_HOME/etc/hadoop
 #for i in `cat /etc/hosts | tail -n +4  | awk '{print $2}'`; do sudo echo -e $1 >> $HADOOP_COMMON_HOME/etc/hadoop/slaves
 #done
 
+sudo chmod -R 777 /usr/lib/hadoop
+
 cd /home/vagrant/hadoop-rel-release-2.7.3
 cp hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/target/*.jar $HADOOP_COMMON_HOME/share/hadoop/yarn
 
@@ -49,10 +51,8 @@ cp $EKETAL_HOME/co.edu.icesi.eketal.parent-master/co.edu.icesi.eketal.lib/target
 
 cp /vagrant/default-config-files/* $HADOOP_COMMON_HOME/etc/hadoop
 
-#sudo chmod -R 777 /usr/lib/hadoop
-
 exec 3>&1 1>~/logs
-for i in `cat ${HADOOP_COMMON_HOME}/etc/hadoop/slaves | head -n -1`; do echo $i; sudo -u vagrant rsync -avxP --exclude=logs $HADOOP_COMMON_HOME/ $i:/home/vagrant/hadoop/; cat /etc/profile.d/hadoop.sh | ssh slave1 "touch hadoop.sh && cat > hadoop.sh && sudo mv hadoop.sh /etc/profile.d"
+for i in `cat ${HADOOP_COMMON_HOME}/etc/hadoop/slaves | head -n -1`; do echo $i; sudo -u vagrant rsync -avxP --exclude=logs $HADOOP_COMMON_HOME/ $i:/home/vagrant/hadoop/; cat /etc/profile.d/hadoop.sh | sudo -u vagrant ssh slave1 "touch hadoop.sh && cat > hadoop.sh && sudo mv hadoop.sh /etc/profile.d && sudo mv /home/vagrant/hadoop/ /usr/lib/hadoop"
 done
 exec 1>&3 3>&-;
 
