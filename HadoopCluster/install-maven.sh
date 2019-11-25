d313811b1071040cc7c3aa6ffe82595cec9556da
camilo040494 <camilo040494@gmail.com>
2019-11-25 08:27:35 -0500

    Update dead link for maven download

--- HadoopCluster/install-maven.sh
+++ HadoopCluster/install-maven.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Installing MAVEN"
-curl -O http://apache.uniminuto.edu/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
-sudo tar -xzvf apache-maven-3.5.2-bin.tar.gz -C /usr/lib
+curl -O curl -O https://www-eu.apache.org/dist/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz
+sudo tar -xzvf apache-maven-3.6.2-bin.tar.gz -C /usr/lib
 sudo touch /etc/profile.d/mvn.sh
 sudo echo "export PATH=$PATH:/usr/lib/apache-maven-3.5.2/bin" >> /etc/profile.d/mvn.sh
 #sudo echo "export PATH=$PATH:/usr/lib/apache-maven-3.5.2/bin" >> /home/vagrant/.bash_profile
